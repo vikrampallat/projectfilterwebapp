77e6116b6196dbbdd884fb40372247975e38ad7a
vikrampallat <76404144+vikrampallat@users.noreply.github.com>
2021-06-24 10:31:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+
+noseX=0;
+noseY=0;
+
+function preload() {
+mustache = loadImage('https://i.postimg.cc/s2X3zjWz/mustachefilter.jpg')
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x -15;
+        noseY = results[0].pose.nose.y +10;
+        console.log("nose x = " + noseX );
+        console.log("nose y = " + noseY );
+    }
+}
+
+function draw() 
+{
+    image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 30, 30);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
